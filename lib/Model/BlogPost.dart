82545d982b70d063263f16a29fa21f6a195f57f6
i4mjad <i4mjad@gmail.com>
2021-11-12 19:21:56 +0400

    edit blog post model

--- lib/Model/BlogPost.dart
+++ lib/Model/BlogPost.dart
@@ -1,33 +1,16 @@
 class BlogPost {
 // ignore: non_constant_identifier_names
-  BlogPost(
-      {this.id,
-      this.title,
-      this.body,
-      this.slug,
-      this.createdAt,
-      this.updatedAt,
-      this.categoryId});
+  BlogPost({
+    this.title,
+    this.postedAt,
+    this.author,
+    this.timeToRead,
+    this.body,
+  });
 
-  final int id;
   final String title;
+  final String postedAt;
+  final String author;
+  final String timeToRead;
   final String body;
-  final String slug;
-  // ignore: non_constant_identifier_names
-  final String createdAt;
-  // ignore: non_constant_identifier_names
-  final String updatedAt;
-  final String categoryId;
-
-  factory BlogPost.fromJson(Map<String, dynamic> json) {
-    return BlogPost(
-      id: json["id"] as int,
-      title: json["title"] as String,
-      body: json["body"] as String,
-      slug: json["contentOwner"] as String,
-      createdAt: json["created_at"] as String,
-      updatedAt: json["updatedAt"] as String,
-      categoryId: json["category_id"] as String,
-    );
-  }
 }
