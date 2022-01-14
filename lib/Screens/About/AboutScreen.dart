d64a3a35701892be0a4fe3bd193328d7715ab467
Amjad Khalfan <58751685+i4mjad@users.noreply.github.com>
2022-01-14 11:33:56 +0400

    remove support app development

--- lib/Screens/About/AboutScreen.dart
+++ lib/Screens/About/AboutScreen.dart
@@ -3,11 +3,8 @@ import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:iconsax/iconsax.dart';
-import 'package:purchases_flutter/object_wrappers.dart';
 import 'package:reboot_app_3/Services/Constants.dart';
 import 'package:reboot_app_3/Localization.dart';
-import 'package:reboot_app_3/Services/PaymentServices.dart';
 import 'package:shared_preferences/shared_preferences.dart';
 import 'package:url_launcher/url_launcher.dart';
 
@@ -86,74 +83,48 @@ class _AboutScreenState extends State<AboutScreen>
                           fontWeight: FontWeight.w500),
                     ),
                     SizedBox(
-                      height: 40,
+                      height: 80,
                     ),
                     Column(
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
-                        Column(
-                          children: [
-                            Row(
-                              crossAxisAlignment: CrossAxisAlignment.center,
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                InkWell(
-                                  onTap: () => launch("https://www.i4mjad.com"),
-                                  child: Container(
-                                    height: 55,
-                                    width: (MediaQuery.of(context).size.width -
-                                            40) *
-                                        0.475,
-                                    decoration: BoxDecoration(
-                                        color: Colors.white,
-                                        borderRadius:
-                                            BorderRadius.circular(12.5),
-                                        border: Border.all(
-                                            color:
-                                                primaryColor.withOpacity(0.5),
-                                            width: 0.25)),
-                                    child: Center(
-                                      child: Text(
-                                          AppLocalizations.of(context).translate(
-                                              'contact-us-through-this-channels'),
-                                          style: kSubTitlesSubsStyle.copyWith(
-                                              fontSize: 14,
-                                              color: primaryColor,
-                                              height: 1)),
-                                    ),
-                                  ),
-                                ),
-                                GestureDetector(
-                                  onTap: () async {
-                                    fetchOffers();
-                                  },
-                                  child: Container(
-                                    height: 55,
-                                    width: (MediaQuery.of(context).size.width -
-                                            40) *
-                                        0.475,
-                                    decoration: BoxDecoration(
-                                      color: Colors.white,
-                                      border: Border.all(
-                                          color: primaryColor.withOpacity(0.5),
-                                          width: 0.25),
-                                      borderRadius: BorderRadius.circular(12.5),
-                                    ),
-                                    child: Center(
-                                      child: Text(
-                                          AppLocalizations.of(context)
-                                              .translate(
-                                                  'support-app-development'),
-                                          style: kSubTitlesSubsStyle.copyWith(
-                                              fontSize: 14,
-                                              color: primaryColor,
-                                              height: 1)),
-                                    ),
-                                  ),
-                                ),
-                              ],
+
+                        InkWell(
+                          onTap: () => launch("https://www.i4mjad.com"),
+                          child: Container(
+                            height: 55,
+                            width: MediaQuery.of(context).size.width - 40,
+                            decoration: BoxDecoration(
+                                color: Colors.white,
+                                borderRadius:
+                                    BorderRadius.circular(12.5),
+                                border: Border.all(
+                                    color:
+                                        primaryColor.withOpacity(0.5),
+                                    width: 0.25)),
+                            child: Center(
+                              child: Text(
+                                  AppLocalizations.of(context).translate(
+                                      'contact-us-through-this-channels'),
+                                  style: kSubTitlesSubsStyle.copyWith(
+                                      fontSize: 14,
+                                      color: primaryColor,
+                                      height: 1)
+
+                              ),
                             ),
-                            SizedBox(height: 20),
-                          ],
+                          ),
+                        ),
+                        SizedBox(height: 12,),
+                        Text(
+                            lang == 'ar' ? 'إذا استفدت من التطبيق, اذكُر والدي - رحمه الله - بدعوة' : '',
+                            style: kSubTitlesSubsStyle.copyWith(
+                                fontSize: 12,
+                                color: accentColor.withOpacity(0.7),
+                                height: 1
+
+                            )
                         ),
                       ],
                     ),
@@ -165,306 +136,5 @@ class _AboutScreenState extends State<AboutScreen>
         ),
       ),
     );
-  }
-
-  supportAppDevelopment(List<Package> offers) async {
-    // print(offers[0].identifier);
-    // print(offers[1].identifier);
-    // print(offers[2].identifier);
-    // print(offers[3].identifier);
-    // print(offers[4].identifier);
-
-    showModalBottomSheet(
-        context: context,
-        builder: (context) {
-          return Padding(
-            padding: EdgeInsets.only(left: 20.0, right: 20, top: 8),
-            child: SingleChildScrollView(
-              child: Column(
-                mainAxisSize: MainAxisSize.max,
-                children: <Widget>[
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 5,
-                        width: MediaQuery.of(context).size.width * 0.1,
-                        decoration: BoxDecoration(
-                            color: Colors.black12,
-                            borderRadius: BorderRadius.circular(50)),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Container(
-                        height: 45,
-                        width: 45,
-                        decoration: BoxDecoration(
-                            color: Colors.redAccent.withOpacity(0.3),
-                            borderRadius: BorderRadius.circular(50)),
-                        child: Center(
-                          child: Icon(
-                            Iconsax.heart,
-                            color: Colors.red,
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                  Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      Text(
-                        AppLocalizations.of(context)
-                            .translate('support-app-development'),
-                        style: kPageTitleStyle.copyWith(
-                            fontSize: 24, color: primaryColor),
-                      )
-                    ],
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  Container(
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        Flexible(
-                          child: Text(
-                            AppLocalizations.of(context)
-                                .translate('support-app-development-p'),
-                            style: kSubTitlesSubsStyle.copyWith(
-                              fontSize: 12,
-                              color: primaryColor.withOpacity(0.7),
-                            ),
-                            textAlign: TextAlign.center,
-                          ),
-                        )
-                      ],
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  ),
-                  //small tip
-                  GestureDetector(
-                    onTap: () {
-                      PaymentServices.purchasePackage(offers[0]);
-                    },
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12.5),
-                            ),
-                            child: Row(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("🤍"),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)
-                                          .translate('small-tip'),
-                                      style: kSubTitlesStyle.copyWith(
-                                          fontSize: 14, color: primaryColor),
-                                    )
-                                  ],
-                                ),
-                                Spacer(),
-                                Text(lang != 'ar'
-                                    ? '\u0024 0.99'
-                                    : '0.99 \u0024')
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  //medium tip
-                  GestureDetector(
-                    onTap: () {
-                      PaymentServices.purchasePackage(offers[1]);
-                    },
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12.5),
-                            ),
-                            child: Row(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("💙"),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)
-                                          .translate('mid-tip'),
-                                      style: kSubTitlesStyle.copyWith(
-                                          fontSize: 14, color: primaryColor),
-                                    )
-                                  ],
-                                ),
-                                Spacer(),
-                                Text(lang != 'ar'
-                                    ? '\u0024 2.99'
-                                    : '2.99 \u0024')
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  //high tip
-                  GestureDetector(
-                    onTap: () {
-                      PaymentServices.purchasePackage(offers[2]);
-                    },
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12.5),
-                            ),
-                            child: Row(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("❤️"),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)
-                                          .translate('high-tip'),
-                                      style: kSubTitlesStyle.copyWith(
-                                          fontSize: 14, color: primaryColor),
-                                    )
-                                  ],
-                                ),
-                                Spacer(),
-                                Text(lang != 'ar'
-                                    ? '\u0024 6.99'
-                                    : '6.99 \u0024')
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 8,
-                  ),
-                  //massive tip
-                  GestureDetector(
-                    onTap: () {
-                      PaymentServices.purchasePackage(offers[3]);
-                    },
-                    child: Container(
-                      child: Column(
-                        children: [
-                          Container(
-                            padding: EdgeInsets.all(12),
-                            decoration: BoxDecoration(
-                              color: Colors.white,
-                              borderRadius: BorderRadius.circular(12.5),
-                            ),
-                            child: Row(
-                              children: [
-                                Row(
-                                  mainAxisAlignment:
-                                      MainAxisAlignment.spaceBetween,
-                                  children: [
-                                    Text("🖤"),
-                                    SizedBox(
-                                      width: 8,
-                                    ),
-                                    Text(
-                                      AppLocalizations.of(context)
-                                          .translate('massive-tip'),
-                                      style: kSubTitlesStyle.copyWith(
-                                          fontSize: 14, color: primaryColor),
-                                    )
-                                  ],
-                                ),
-                                Spacer(),
-                                Text(lang != 'ar'
-                                    ? '\u0024 9.99'
-                                    : '9.99 \u0024')
-                              ],
-                            ),
-                          )
-                        ],
-                      ),
-                    ),
-                  ),
-                  SizedBox(
-                    height: 20,
-                  )
-                ],
-              ),
-            ),
-          );
-        });
-  }
-
-  Future fetchOffers() async {
-    final offerings = await PaymentServices.fetchOffers();
-
-    if (offerings == null) {
-      ScaffoldMessenger.of(context).showSnackBar(SnackBar(
-        content: Text(
-          AppLocalizations.of(context).translate('no-donations-offer'),
-          style: kSubTitlesStyle.copyWith(
-              fontSize: 12, color: Colors.white, fontWeight: FontWeight.bold),
-        ),
-        duration: Duration(seconds: 2),
-      ));
-    } else {
-      final packages = offerings
-          .map((offer) => offer.availablePackages)
-          .expand((element) => element)
-          .toList();
-
-      supportAppDevelopment(packages);
-    }
   }
 }
--- lib/Services/PaymentServices.dart
+++ lib/Services/PaymentServices.dart
@@ -1,33 +1,30 @@
-import 'package:flutter/services.dart';
-import 'package:purchases_flutter/purchases_flutter.dart';
-
-class PaymentServices {
-  static const _apiKey = 'BsKLbFbMxsauIzrGbFJAqEcCSTpoTraR';
-
-  static Future<void> initPayment() async {
-    await Purchases.setDebugLogsEnabled(true);
-    await Purchases.setup(_apiKey);
-  }
-
-  static Future<List<Offering>> fetchOffers() async {
-    try {
-      final offerings = await Purchases.getOfferings();
-      final current = offerings.current;
-
-      return current == null ? [] : [current];
-    } on PlatformException catch (e) {
-      print(e.code);
-
-      return [];
-    }
-  }
-
-  static Future<bool> purchasePackage(Package package) async {
-    try {
-      await Purchases.purchasePackage(package);
-      return true;
-    } catch (e) {
-      return false;
-    }
-  }
-}
+// class PaymentServices {
+//   static const _apiKey = 'BsKLbFbMxsauIzrGbFJAqEcCSTpoTraR';
+//
+//   static Future<void> initPayment() async {
+//     await Purchases.setDebugLogsEnabled(true);
+//     await Purchases.setup(_apiKey);
+//   }
+//
+//   static Future<List<Offering>> fetchOffers() async {
+//     try {
+//       final offerings = await Purchases.getOfferings();
+//       final current = offerings.current;
+//
+//       return current == null ? [] : [current];
+//     } on PlatformException catch (e) {
+//       print(e.code);
+//
+//       return [];
+//     }
+//   }
+//
+//   static Future<bool> purchasePackage(Package package) async {
+//     try {
+//       await Purchases.purchasePackage(package);
+//       return true;
+//     } catch (e) {
+//       return false;
+//     }
+//   }
+// }
