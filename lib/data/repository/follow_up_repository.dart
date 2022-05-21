8d5f7d4c24df3fae36385a5bb11e7e9174d77aa6
Amjad Khalfan <58751685+i4mjad@users.noreply.github.com>
2022-05-21 18:15:23 +0400

    feat, add method to get follow up data

--- lib/data/repository/follow_up_repository.dart
+++ lib/data/repository/follow_up_repository.dart
@@ -5,13 +5,13 @@ import 'package:reboot_app_3/data/models/user_profile.dart';
 import 'package:reboot_app_3/data/web_services/firebase_service.dart';
 
 class FirebaseFollowUpRepository implements FollowUpRepository {
-  FirebaseService _firebaseService =
-      FirebaseService(FirebaseFirestore.instance);
-  String userUid = FirebaseAuth.instance.currentUser.uid;
-
+  FirebaseService _firebaseService = FirebaseService(
+      FirebaseFirestore.instance, FirebaseAuth.instance.currentUser.uid);
   @override
-  Stream<FollowUpData> relapses() {
-    // TODO: implement relapses
-    throw UnimplementedError();
+  getFollowUpData() {
+    _firebaseService.streamUserData().listen((event) async {
+      var sh = event as DocumentSnapshot;
+      return await FollowUpData.fromSnapshot(sh.data());
+    });
   }
 }
