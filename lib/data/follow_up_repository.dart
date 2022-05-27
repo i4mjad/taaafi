d90a3bbb785a05d56a8afe680c35c763d6a08d68
Amjad Khalfan <58751685+i4mjad@users.noreply.github.com>
2022-05-27 17:42:55 +0400

    refactor, remove old files

--- lib/data/follow_up_repository.dart
+++
@@ -1,5 +0,0 @@
-import 'models/user_profile.dart';
-
-abstract class FollowUpRepository {
-  FollowUpData getFollowUpData();
-}
--- lib/data/user_repository.dart
+++
@@ -1,9 +0,0 @@
-abstract class UserRepository {
-  Future<bool> isAuthenticated();
-
-  Future<void> authenticateAnonymously();
-
-  Future<String> getUserId();
-
-  Future<void> createNewUserDocument();
-}
