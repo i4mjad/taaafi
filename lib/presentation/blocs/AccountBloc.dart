e98fa83ab3c2dcca0a760b9680e32fbab6c6be6d
Amjad Khalfan <58751685+i4mjad@users.noreply.github.com>
2022-06-05 23:23:41 +0400

    feat, create and use AccountBloc instead of using FollowYourRebootBloc

---
+++ lib/presentation/blocs/AccountBloc.dart
@@ -0,0 +1,25 @@
+import 'dart:async';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:reboot_app_3/bloc_provider.dart';
+
+import 'package:reboot_app_3/repository/db.dart';
+import 'package:rxdart/subjects.dart';
+
+class AccountBloc implements CustomBlocBase {
+  AccountBloc() {
+    db.initStream().listen((data) => _inFirestore.add(data));
+  }
+
+  final _firestoreController = BehaviorSubject<DocumentSnapshot>();
+  Stream<DocumentSnapshot> get outFirestore => _firestoreController.stream;
+  Sink<DocumentSnapshot> get _inFirestore => _firestoreController.sink;
+
+  @override
+  void dispose() async {
+    return _firestoreController.close();
+  }
+
+  Future<void> createNewData(DateTime selectedDate) async {
+    return await db.createNewData(selectedDate);
+  }
+}
--- lib/presentation/screens/account/account_screen.dart
+++ lib/presentation/screens/account/account_screen.dart
@@ -2,16 +2,16 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:reboot_app_3/shared/localization/localization.dart';
+import 'package:reboot_app_3/shared/localization/localization_services.dart';
 import 'package:reboot_app_3/bloc_provider.dart';
-import 'package:reboot_app_3/presentation/blocs/follow_your_reboot_bloc.dart';
+import 'package:reboot_app_3/presentation/blocs/AccountBloc.dart';
 import 'package:reboot_app_3/presentation/screens/follow_your_reboot/widgets/new_user_widgets.dart';
 import 'package:reboot_app_3/shared/components/change_locale_bottomsheet.dart';
 import 'package:reboot_app_3/shared/constants/textstyles_constants.dart';
 import 'package:provider/provider.dart';
 import 'package:reboot_app_3/presentation/screens/auth/login_screen.dart';
 import 'package:reboot_app_3/shared/constants/constants.dart';
-import 'package:reboot_app_3/shared/localization/localization.dart';
-import 'package:reboot_app_3/shared/localization/localization_services.dart';
 import 'package:reboot_app_3/shared/services/auth_service.dart';
 import 'package:reboot_app_3/shared/services/notification_service.dart';
 
@@ -44,7 +44,7 @@ class _AccountScreenState extends State<AccountScreen>
 
   @override
   Widget build(BuildContext context) {
-    final bloc = CustomBlocProvider.of<FollowYourRebootBloc>(context);
+    final bloc = CustomBlocProvider.of<AccountBloc>(context);
     return Scaffold(
         backgroundColor: seconderyColor.withOpacity(0.2),
         body: Padding(
@@ -281,103 +281,114 @@ class _AccountScreenState extends State<AccountScreen>
         ));
   }
 
-  void resetUserDialog(BuildContext context, FollowYourRebootBloc bloc) {
+  resetUserDialog(BuildContext context, AccountBloc bloc) {
     showModalBottomSheet(
       context: context,
-      enableDrag: false,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.only(
             topRight: Radius.circular(30.0), topLeft: Radius.circular(30.0)),
       ),
-      builder: (context) {
-        return Container(
-            height: MediaQuery.of(context).size.height * 0.35,
-            width: MediaQuery.of(context).size.width,
-            padding: EdgeInsets.all(16),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Container(
-                  padding: EdgeInsets.all(8),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(100),
-                    color: primaryColor.withOpacity(0.1),
-                  ),
-                  child: Icon(
-                    Iconsax.calendar_tick,
-                    color: primaryColor,
-                    size: 32,
-                  ),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  'تابع تعافيك',
-                  style: kHeadlineStyle.copyWith(
-                      fontWeight: FontWeight.bold, color: primaryColor),
-                ),
-                SizedBox(
-                  height: 8,
-                ),
-                Text(
-                  'لمتابعة تعافيك من إدمان الإباحية، اختر تاريخ اليوم الذي تود بدء المتابعة منه',
-                  style: kBodyStyle.copyWith(height: 1.2),
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 24,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    GestureDetector(
-                      onTap: () async {
-                        var selectedDate = await getDateTime(context);
-                        await bloc.createNewData(selectedDate);
-                      },
-                      child: Container(
-                        height: 80,
-                        width:
-                            ((MediaQuery.of(context).size.width - 40) - 8) / 2,
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            color: accentColor.withOpacity(0.2),
-                            borderRadius: BorderRadius.circular(12.5)),
-                        child: Center(
-                          child: Text(
-                            "يوم معين",
-                            style: kTitleSeconderyStyle,
-                          ),
-                        ),
-                      ),
+      builder: (BuildContext context) {
+        return StatefulBuilder(
+            builder: (BuildContext context, StateSetter bsState) {
+          return Container(
+              height: MediaQuery.of(context).size.height * 0.35,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Container(
+                    padding: EdgeInsets.all(8),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(100),
+                      color: primaryColor.withOpacity(0.1),
                     ),
-                    GestureDetector(
-                      onTap: () async {
-                        var today = getToday();
-                        await bloc.createNewData(today);
-                      },
-                      child: Container(
-                        height: 80,
-                        width:
-                            ((MediaQuery.of(context).size.width - 40) - 8) / 2,
-                        padding: EdgeInsets.all(8),
-                        decoration: BoxDecoration(
-                            color: accentColor,
-                            borderRadius: BorderRadius.circular(12.5)),
-                        child: Center(
-                          child: Text(
-                            "اليوم",
-                            style: kTitleSeconderyStyle.copyWith(
-                                color: Colors.white),
+                    child: Icon(
+                      Iconsax.calendar_tick,
+                      color: primaryColor,
+                      size: 32,
+                    ),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    AppLocalizations.of(context)
+                        .translate('delete-user-dialog-title'),
+                    style: kHeadlineStyle.copyWith(
+                        fontWeight: FontWeight.bold, color: primaryColor),
+                  ),
+                  SizedBox(
+                    height: 8,
+                  ),
+                  Text(
+                    AppLocalizations.of(context)
+                        .translate("delete-user-dialog-content"),
+                    style: kBodyStyle.copyWith(height: 1.2),
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 24,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      GestureDetector(
+                        onTap: () async {
+                          var selectedDate = await getDateTime(context);
+                          await bloc
+                              .createNewData(selectedDate)
+                              .then((value) => Navigator.pop(context));
+                        },
+                        child: Container(
+                          height: 80,
+                          width:
+                              ((MediaQuery.of(context).size.width - 40) - 8) /
+                                  2,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: accentColor.withOpacity(0.2),
+                              borderRadius: BorderRadius.circular(12.5)),
+                          child: Center(
+                            child: Text(
+                              AppLocalizations.of(context)
+                                  .translate("specific-day"),
+                              style: kTitleSeconderyStyle,
+                            ),
                           ),
                         ),
                       ),
-                    )
-                  ],
-                )
-              ],
-            ));
+                      GestureDetector(
+                        onTap: () async {
+                          var today = getToday();
+                          await bloc
+                              .createNewData(today)
+                              .then((value) => Navigator.pop(context));
+                        },
+                        child: Container(
+                          height: 80,
+                          width:
+                              ((MediaQuery.of(context).size.width - 40) - 8) /
+                                  2,
+                          padding: EdgeInsets.all(8),
+                          decoration: BoxDecoration(
+                              color: accentColor,
+                              borderRadius: BorderRadius.circular(12.5)),
+                          child: Center(
+                            child: Text(
+                              AppLocalizations.of(context).translate("today"),
+                              style: kTitleSeconderyStyle.copyWith(
+                                  color: Colors.white),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
+                  )
+                ],
+              ));
+        });
       },
     );
   }
@@ -389,7 +400,6 @@ class AccountScreenScreenAuthenticationWrapper extends StatelessWidget {
     final user = context.watch<User>();
     return user == null
         ? LoginScreen()
-        : CustomBlocProvider(
-            bloc: FollowYourRebootBloc(), child: AccountScreen());
+        : CustomBlocProvider(bloc: AccountBloc(), child: AccountScreen());
   }
 }
