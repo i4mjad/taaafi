80ff9115bb5f4002d25677c45c5ecedc2228490e
Amjad Khalfan <i4mjad@gmail.com>
2022-06-10 14:02:03 +0400

    #1, cleanup, remove comments in auth service

--- lib/Shared/services/auth_service.dart
+++ lib/Shared/services/auth_service.dart
@@ -10,34 +10,32 @@ class GoogleAuthenticationService extends ChangeNotifier {
   Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
 
   Future<UserCredential> signInWithGoogle() async {
-    // Initiate the auth procedure
     final GoogleSignInAccount googleUser =
         await GoogleSignIn(scopes: ["email"]).signIn();
-    // fetch the auth details from the request made earlier
+
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
-    // Create a new credential for signing in with google
+
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
-    // Once signed in, return the UserCredential
+
     return await FirebaseAuth.instance.signInWithCredential(credential);
   }
 
   Future<UserCredential> reauthenticateWithsignInWithGoogle() async {
-    // Initiate the auth procedure
     final GoogleSignInAccount googleUser =
         await GoogleSignIn(scopes: ["email"]).signIn();
-    // fetch the auth details from the request made earlier
+
     final GoogleSignInAuthentication googleAuth =
         await googleUser.authentication;
-    // Create a new credential for signing in with google
+
     final credential = GoogleAuthProvider.credential(
       accessToken: googleAuth.accessToken,
       idToken: googleAuth.idToken,
     );
-    // Once signed in, return the UserCredential
+
     return await FirebaseAuth.instance.currentUser
         .reauthenticateWithCredential(credential);
   }
