468600178d6dcf60e80942b5d637b3c4735abcc8
Amjad Khalfan <i4mjad@gmail.com>
2022-06-15 17:43:25 +0400

    #14, refactor, restore deleted widgets

--- lib/presentation/screens/account/account_screen.dart
+++ lib/presentation/screens/account/account_screen.dart
@@ -1,6 +1,7 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
+import 'package:reboot_app_3/presentation/screens/account/delete_account.dart';
 import 'package:reboot_app_3/shared/localization/localization.dart';
 import 'package:reboot_app_3/shared/localization/localization_services.dart';
 import 'package:reboot_app_3/bloc_provider.dart';
@@ -14,9 +15,7 @@ import 'package:reboot_app_3/shared/constants/constants.dart';
 import 'package:reboot_app_3/shared/services/auth_service.dart';
 import 'package:reboot_app_3/shared/services/notification_service.dart';
 
-import 'Widgets/delete_account_bottomsheet.dart';
 
-import 'Widgets/user_profile_card.dart';
 
 class AccountScreen extends StatefulWidget {
   const AccountScreen({
@@ -389,6 +388,57 @@ class _AccountScreenState extends State<AccountScreen>
               ));
         });
       },
+    );
+  }
+}
+
+class UserProfileCard extends StatelessWidget {
+  UserProfileCard({Key key, String lang}) : super(key: key);
+
+  final User user = FirebaseAuth.instance.currentUser;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.center,
+      children: [
+        Column(
+          children: [
+            Container(
+              padding: EdgeInsets.all(40),
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(500),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.grey.withOpacity(0.5),
+                    spreadRadius: .25,
+                    blurRadius: 7,
+                    offset: Offset(0, 2), // changes position of shadow
+                  ),
+                ],
+              ),
+              child: Icon(
+                Iconsax.user,
+                size: 56,
+                color: primaryColor,
+              ),
+            ),
+            SizedBox(height: 16),
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text(
+                  this.user.displayName,
+                  style: kTitlePrimeryStyle,
+                ),
+                SizedBox(height: 8),
+                Text(this.user.email.toUpperCase(), style: kCaptionStyle),
+              ],
+            ),
+          ],
+        ),
+      ],
     );
   }
 }
