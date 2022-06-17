fc82f06958d6ba28c4535b8c71b953e3723f5474
Amjad Khalfan <i4mjad@gmail.com>
2022-06-17 19:29:38 +0400

    #21, feat, create initial design for explore widget

--- lib/presentation/screens/home/widgets/explore_widget.dart
+++ lib/presentation/screens/home/widgets/explore_widget.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
-import 'package:reboot_app_3/data/models/ExploreContent.dart';
+import 'package:reboot_app_3/data/models/WelcomeContent.dart';
 import 'package:reboot_app_3/shared/constants/constants.dart';
 import 'package:reboot_app_3/shared/constants/textstyles_constants.dart';
 import 'package:reboot_app_3/shared/localization/localization.dart';
@@ -10,70 +10,106 @@ class ExploreWidget extends StatelessWidget {
     Key key,
   }) : super(key: key);
 
-
-
   @override
   Widget build(BuildContext context) {
     return Column(
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text(AppLocalizations.of(context).translate("explore"), style: kSubTitlesStyle,),
-        SizedBox(height: 8,),
+        Text(
+          AppLocalizations.of(context).translate('explore'),
+          style: kSubTitlesStyle,
+        ),
+        SizedBox(
+          height: 8,
+        ),
         Row(
           children: [
-            Builder(
-                builder: (BuildContext context) {
-                  return Expanded(
-                    child: Container(
-                      height: 150,
-                      child: Column(
-                        children: [
-                          Expanded(child: Builder(builder: (context) {
-                            return ListView.separated(
-                              scrollDirection: Axis.horizontal,
-                              itemCount: 3,
-                              itemBuilder:(BuildContext context, int index) {
-                                return GestureDetector(
-                                  onTap: (){
-                                    HapticFeedback.heavyImpact();
-                                  },
-                                  child: Container(
-                                    padding: EdgeInsets.all(16),
-                                    width: MediaQuery.of(context).size.width - 150,
-                                    height: 125,
-                                    decoration: BoxDecoration(
-                                      color: FAKE_EXPLORE_CONTENT[index].bgColor,
-                                      border: Border.all(color: primaryColor, width: 0.25),
-                                      borderRadius: BorderRadius.circular(12.5),
-                                    ),
-                                    child: Column(
-                                      crossAxisAlignment: CrossAxisAlignment.start,
-                                      mainAxisAlignment: MainAxisAlignment.end,
+            Builder(builder: (BuildContext context) {
+              return Expanded(
+                child: Container(
+                  height: 150,
+                  child: Column(
+                    children: [
+                      Expanded(child: Builder(builder: (context) {
+                        return ListView.separated(
+                          scrollDirection: Axis.horizontal,
+                          itemCount: 3,
+                          itemBuilder: (BuildContext context, int index) {
+                            return GestureDetector(
+                              onTap: () {
+                                HapticFeedback.lightImpact();
+                              },
+                              child: Container(
+                                padding: EdgeInsets.all(16),
+                                width: MediaQuery.of(context).size.width * 0.35,
+                                decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                        color: primaryColor, width: 0.25),
+                                    borderRadius: BorderRadius.circular(12.5)),
+                                child: Column(
+                                  crossAxisAlignment: CrossAxisAlignment.start,
+                                  children: [
+                                    Row(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.spaceBetween,
                                       children: [
-                                        Text(
-                                            FAKE_EXPLORE_CONTENT[index].title,
-                                          style: kSubTitlesStyle.copyWith(
-                                            color: FAKE_EXPLORE_CONTENT[index].txtColor
+                                        CircleAvatar(
+                                          backgroundColor: primaryColor,
+                                          child: FAKE_CONTENT[index].icon,
+                                        ),
+                                        Container(
+                                          padding: EdgeInsets.all(8),
+                                          decoration: BoxDecoration(
+                                            color: accentColor,
+                                            borderRadius:
+                                                BorderRadius.circular(8),
                                           ),
-                                        )
+                                          child: Text(
+                                              FAKE_CONTENT[index].subtitle,
+                                              style:
+                                                  kSubTitlesSubsStyle.copyWith(
+                                                      color: Colors.white,
+                                                      fontSize: 8)),
+                                        ),
                                       ],
                                     ),
-                                  ),
-                                );
-                              }, separatorBuilder: (BuildContext context, int index) {
-                              return SizedBox(width: 16);
-                            },);
-                          })),
-                        ],
-                      ),
-                    ),
-                  );
-                }
-            ),
-
+                                    Spacer(),
+                                    Container(
+                                      child: Row(
+                                        crossAxisAlignment:
+                                            CrossAxisAlignment.center,
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.start,
+                                        children: [
+                                          Flexible(
+                                              child: Text(
+                                                  FAKE_CONTENT[index].title,
+                                                  style: kSubTitlesSubsStyle
+                                                      .copyWith(
+                                                          color: primaryColor,
+                                                          fontWeight:
+                                                              FontWeight.w500)))
+                                        ],
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                            );
+                          },
+                          separatorBuilder: (BuildContext context, int index) {
+                            return SizedBox(width: 16);
+                          },
+                        );
+                      })),
+                    ],
+                  ),
+                ),
+              );
+            }),
           ],
         ),
-
       ],
     );
   }
