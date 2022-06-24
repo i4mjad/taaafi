430fb1dc2334bd0be87291b39ea94c33804014d3
Amjad Khalfan <i4mjad@gmail.com>
2022-06-24 11:43:04 +0400

    AB#20, wip, create themes for light & dark mode

---
+++ lib/shared/components/app-themes.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:reboot_app_3/shared/constants/constants.dart';
+
+CustomTheme currentTheme = CustomTheme();
+
+class CustomTheme with ChangeNotifier {
+  static bool _isDarkTheme = false;
+  ThemeMode get currentTheme => _isDarkTheme ? ThemeMode.dark : ThemeMode.light;
+
+  void toggleTheme() {
+    _isDarkTheme = !_isDarkTheme;
+    notifyListeners();
+  }
+
+  static ThemeData get lightTheme {
+    return ThemeData(
+      primaryColor: primaryColor,
+      backgroundColor: Colors.white,
+      scaffoldBackgroundColor: seconderyColor,
+      appBarTheme: AppBarTheme(
+        backgroundColor: seconderyColor,
+      ),
+      textTheme: TextTheme(
+        headline1: TextStyle(color: Colors.black),
+        headline2: TextStyle(color: Colors.black),
+        bodyText1: TextStyle(color: Colors.black),
+        bodyText2: TextStyle(color: Colors.black),
+      ),
+    );
+  }
+
+  static ThemeData get darkTheme {
+    return ThemeData(
+      primaryColor: Colors.white,
+      scaffoldBackgroundColor: darkBackgroundColor,
+      bottomAppBarColor: darkBackgroundColor,
+      appBarTheme: AppBarTheme(
+        backgroundColor: darkBackgroundColor,
+      ),
+      textTheme: TextTheme(
+        headline1: TextStyle(color: Colors.white),
+        headline2: TextStyle(color: Colors.white),
+        bodyText1: TextStyle(color: Colors.white),
+        bodyText2: TextStyle(color: Colors.white),
+      ),
+    );
+  }
+}
