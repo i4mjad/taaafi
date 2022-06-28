635b42638b602fb7e6cd9980b16ecf979ae013f2
Amjad Khalfan <i4mjad@gmail.com>
2022-06-28 18:15:58 +0400

    chore, remove unused imports

--- lib/Shared/services/routing/custom_router.dart
+++ lib/Shared/services/routing/custom_router.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:reboot_app_3/Presentation/screens/about/about_screen.dart';
-import 'package:reboot_app_3/main.dart';
 import 'package:reboot_app_3/presentation/screens/account/account_screen.dart';
 import 'package:reboot_app_3/presentation/screens/auth/login_screen.dart';
 import 'package:reboot_app_3/presentation/screens/community/community_screen.dart';
