f0f6611efbafb28510d179b6b67cc0aa2bb6586f
Amjad Khalfan <i4mjad@gmail.com>
2023-05-19 20:28:07 +0400

    todos, create todos for investigating about flutter unit testing

---
+++ test/builders/followup_repository_builder.dart
@@ -0,0 +1,12 @@
+import 'package:reboot_app_3/repository/follow_up_data_repository.dart';
+
+//TODO: Investigate about best practices for creating builders
+// class FollowUpRepositoryTestDataBuilder {
+  
+//   FirebaseFollowUpDataRepository build() {
+//     return FirebaseFollowUpDataRepository();
+//   }
+// }
+
+// FirebaseFirestore _db;
+//   IUserContext _userContext;
