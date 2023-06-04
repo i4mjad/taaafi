b8c81ef931c8d236604fae835b069e0cffcbd68e
Amjad Khalfan <i4mjad@gmail.com>
2023-06-04 20:39:42 +0400

    refactor, pull relapses by day of week widget to separate file

--- lib/presentation/screens/follow_your_reboot/follow_your_reboot_screen.dart
+++ lib/presentation/screens/follow_your_reboot/follow_your_reboot_screen.dart
@@ -5,6 +5,7 @@ import 'package:reboot_app_3/presentation/screens/follow_your_reboot/follow_up_s
 import 'package:reboot_app_3/presentation/screens/follow_your_reboot/follow_your_reboot_widgets.dart';
 import 'package:reboot_app_3/presentation/screens/follow_your_reboot/widgets/followup_calender.dart';
 import 'package:reboot_app_3/presentation/screens/follow_your_reboot/widgets/general_status_widget.dart';
+import 'package:reboot_app_3/presentation/screens/follow_your_reboot/widgets/relapses_by_day_of_week_widget.dart';
 import 'package:reboot_app_3/shared/components/custom-app-bar.dart';
 import 'package:reboot_app_3/shared/constants/textstyles_constants.dart';
 import 'package:reboot_app_3/shared/localization/localization.dart';
