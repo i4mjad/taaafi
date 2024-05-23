edf7129f7277a1c6734fc1e9bdecd73574a6a5b7
Amjad Khalfan <i4mjad@gmail.com>
2024-05-23 16:07:03 +0400

    chore, create environment variables for firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyDTqyzIxVboEDvW3g5S_zCnGK0smR-jqg8',
+    authDomain: 'rebootapp-37a30.firebaseapp.com',
+    databaseURL: 'https://rebootapp-37a30.firebaseio.com',
+    projectId: 'rebootapp-37a30',
+    storageBucket: 'rebootapp-37a30.appspot.com',
+    messagingSenderId: '364568176835'
+  }
+};
