ad2d402d6867f87cd2ff4ca1a9a5a04db5c36ce1
Amjad Khalfan <i4mjad@gmail.com>
2024-06-11 18:48:00 +0400

    chore, create features starting files

---
+++ lib/features/authentication/repositories/auth_repository.dart
@@ -0,0 +1,9 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:riverpod_annotation/riverpod_annotation.dart';
+
+part 'auth_repository.g.dart';
+
+@riverpod
+FirebaseAuth firebaseAuth(ref) {
+  return FirebaseAuth.instance;
+}
---
+++ lib/features/authentication/repositories/auth_repository.g.dart
@@ -0,0 +1,24 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'auth_repository.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$firebaseAuthHash() => r'cff15592c2b2540ecca81d6450d8034b91548302';
+
+/// See also [firebaseAuth].
+@ProviderFor(firebaseAuth)
+final firebaseAuthProvider = AutoDisposeProvider<FirebaseAuth>.internal(
+  firebaseAuth,
+  name: r'firebaseAuthProvider',
+  debugGetCreateSourceHash:
+      const bool.fromEnvironment('dart.vm.product') ? null : _$firebaseAuthHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef FirebaseAuthRef = AutoDisposeProviderRef<FirebaseAuth>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
