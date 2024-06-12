a6d4b1ffe66e973ded9c54ddc08604908b12c9f2
Amjad Khalfan <i4mjad@gmail.com>
2024-06-12 17:46:46 +0400

    chore, create auth repository

--- lib/features/authentication/repositories/auth_repository.dart
+++ lib/features/authentication/repositories/auth_repository.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:reboot_app_3/providers/user/auth_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_repository.g.dart';
@@ -6,4 +7,26 @@ part 'auth_repository.g.dart';
 @riverpod
 FirebaseAuth firebaseAuth(ref) {
   return FirebaseAuth.instance;
+}
+
+class AuthRepository {
+  AuthRepository(this._auth);
+  final FirebaseAuth _auth;
+
+  Stream<User?> authStateChanges() => _auth.authStateChanges();
+  User? get currentUser => _auth.currentUser;
+
+  Future<void> signInAnonymously() {
+    return _auth.signInAnonymously();
+  }
+}
+
+@riverpod
+AuthRepository authRepository(ref) {
+  return AuthRepository(ref.watch(firebaseAuthProvider));
+}
+
+@riverpod
+Stream<User?> authStateChanges(ref) {
+  return ref.watch(authRepositoryProvider).authStateChanges();
 }
--- lib/features/authentication/repositories/auth_repository.g.dart
+++ lib/features/authentication/repositories/auth_repository.g.dart
@@ -20,5 +20,35 @@ final firebaseAuthProvider = AutoDisposeProvider<FirebaseAuth>.internal(
 );
 
 typedef FirebaseAuthRef = AutoDisposeProviderRef<FirebaseAuth>;
+String _$authRepositoryHash() => r'bbf1b4fab35df122de2952f227f490aaf2acfb2f';
+
+/// See also [authRepository].
+@ProviderFor(authRepository)
+final authRepositoryProvider = AutoDisposeProvider<AuthRepository>.internal(
+  authRepository,
+  name: r'authRepositoryProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authRepositoryHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthRepositoryRef = AutoDisposeProviderRef<AuthRepository>;
+String _$authStateChangesHash() => r'6b6f913c1386b8088ab192ff26f60f730ca9cdeb';
+
+/// See also [authStateChanges].
+@ProviderFor(authStateChanges)
+final authStateChangesProvider = AutoDisposeStreamProvider<User?>.internal(
+  authStateChanges,
+  name: r'authStateChangesProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$authStateChangesHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef AuthStateChangesRef = AutoDisposeStreamProviderRef<User?>;
 // ignore_for_file: type=lint
 // ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
