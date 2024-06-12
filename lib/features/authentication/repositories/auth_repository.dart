6b221d158bbb7c7a9af02a4958fdb9f91cb29396
Amjad Khalfan <i4mjad@gmail.com>
2024-06-12 17:49:39 +0400

    clean, remove unused imports

--- lib/features/authentication/repositories/auth_repository.dart
+++ lib/features/authentication/repositories/auth_repository.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_auth/firebase_auth.dart';
-import 'package:reboot_app_3/providers/user/auth_provider.dart';
 import 'package:riverpod_annotation/riverpod_annotation.dart';
 
 part 'auth_repository.g.dart';
