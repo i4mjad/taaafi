4f0b1d5f23e08e9ceb62f544841eeb5f2353df90
Amjad Khalfan <i4mjad@gmail.com>
2024-06-12 19:43:33 +0400

    feat, create the new main screens

---
+++ lib/features/account/presentation/account_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//TODO: to be updated after the migeration from old account screen
+//! RENAME THIS
+class UpdatedAccountScreen extends StatelessWidget {
+  const UpdatedAccountScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/features/home/presentation/home_screen.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+//TODO: to be updated after the migeration from old account screen
+//! RENAME THIS
+class UpdatedHomeScreen extends StatelessWidget {
+  const UpdatedHomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
