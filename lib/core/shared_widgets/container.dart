e32d440d58c1fa2d8b70f06f6129f272d40b67e4
Amjad Khalfan <i4mjad@gmail.com>
2024-06-19 17:55:00 +0400

    chore, create a reusable container component

---
+++ lib/core/shared_widgets/container.dart
@@ -0,0 +1,46 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+
+class WidgetsContainer extends StatelessWidget {
+  final Widget child;
+  final double? width;
+  final EdgeInsets? padding;
+  final Color? backgroundColor;
+  final BorderRadius? borderRadius;
+  final BorderSide? borderSide;
+  final double? cornerSmoothing;
+
+  WidgetsContainer({
+    required this.child,
+    this.width,
+    this.padding,
+    this.backgroundColor,
+    this.borderRadius,
+    this.borderSide,
+    this.cornerSmoothing,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    return Container(
+      width: width ?? MediaQuery.of(context).size.width,
+      padding: padding ?? EdgeInsets.all(16),
+      decoration: ShapeDecoration(
+        color: backgroundColor ?? theme.primaryColor.withOpacity(0.1),
+        shape: SmoothRectangleBorder(
+          borderRadius: SmoothBorderRadius(
+            cornerRadius: (borderRadius ?? BorderRadius.circular(15)).topLeft.x,
+            cornerSmoothing: cornerSmoothing ?? 1,
+          ),
+          side: borderSide ??
+              BorderSide(
+                color: theme.primaryColor.withOpacity(0.4),
+                width: 1.0,
+              ),
+        ),
+      ),
+      child: child,
+    );
+  }
+}
