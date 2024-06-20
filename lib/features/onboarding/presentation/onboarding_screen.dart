ad5189a1e7a731bf172e1b5451297264414e7dfc
Amjad Khalfan <i4mjad@gmail.com>
2024-06-20 22:23:41 +0400

    feat wip, implement onboarding screen without translation

--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -1,25 +1,158 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
-import 'package:reboot_app_3/core/routing/app_routes.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:reboot_app_3/core/localization/localization.dart';
 import 'package:reboot_app_3/core/routing/route_names.dart';
+import 'package:reboot_app_3/core/shared_widgets/container.dart';
+import 'package:reboot_app_3/core/theming/app-themes.dart';
+import 'package:reboot_app_3/core/theming/custom_theme_data.dart';
+import 'package:reboot_app_3/core/theming/spacing.dart';
 import 'package:reboot_app_3/core/theming/text_styles.dart';
-import 'package:reboot_app_3/features/authentication/repositories/auth_repository.dart';
 
 class OnBoardingScreen extends ConsumerWidget {
   const OnBoardingScreen({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final authRepository = ref.watch(authRepositoryProvider);
+    final theme = CustomThemeInherited.of(context);
     return Scaffold(
-      body: Center(
-          child: PrimaryButton(
-        text: "Log In",
-        onPressed: () {
-          context.goNamed(RouteNames.login.name);
-        },
-      )),
+      backgroundColor: theme.backgroundColor,
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Expanded(
+                child: SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      verticalSpace(Spacing.points40),
+                      Container(
+                        height: 150,
+                        width: 150,
+                        decoration: BoxDecoration(
+                            color: theme.primary[50],
+                            borderRadius: BorderRadius.circular(100),
+                            border: Border.all(
+                                color: theme.primary[100]!, width: 1.5)),
+                        child: Icon(
+                          LucideIcons.heart,
+                          size: 42,
+                        ),
+                      ),
+                      verticalSpace(Spacing.points28),
+                      Text(
+                        AppLocalizations.of(context)
+                            .translate('taaafi-platform'),
+                        style: TextStyles.h2.copyWith(
+                          color: theme.primary[600],
+                        ),
+                      ),
+                      verticalSpace(Spacing.points28),
+                      OnboardingSection(
+                        icon: LucideIcons.users,
+                        title: 'تابع تعافيك',
+                        description:
+                            'تعرف على التجارب المختلفة وشارك تجربتك مع مجموعة من المتعافين لتبادل الخبرات واكتشاف التحديات في رحلة التعافي',
+                      ),
+                      verticalSpace(Spacing.points32),
+                      OnboardingSection(
+                          icon: LucideIcons.lock,
+                          title: 'الخزنة',
+                          description:
+                              'يمكنك مشاركة رحلة تعافيك والتحديات التي تواجهها بسرية تامة بدون إظهار هويتك'),
+                      verticalSpace(Spacing.points32),
+                      OnboardingSection(
+                        icon: LucideIcons.trophy,
+                        title: 'الزمالة',
+                        description:
+                            'تحدَ زملائك في رحلة التعافي لتحقيق مراحل متقدمة في رحلة التعافي',
+                      ),
+                      verticalSpace(Spacing.points32),
+                      OnboardingSection(
+                        icon: LucideIcons.fileStack,
+                        title: 'تابع تحديثات زملائك',
+                        description:
+                            'تابع تحديثات من قبل زملائك في الزمالة وساعدهم واستفد من خبراتهم في التعافي',
+                      ),
+                      verticalSpace(Spacing.points24),
+                    ],
+                  ),
+                ),
+              ),
+              GestureDetector(
+                onTap: () => context.goNamed(RouteNames.login.name),
+                child: WidgetsContainer(
+                  backgroundColor: theme.primary[600],
+                  width: MediaQuery.of(context).size.width - 64,
+                  child: Center(
+                    child: Text(
+                      AppLocalizations.of(context).translate('login'),
+                      style: TextStyles.footnoteSelected.copyWith(
+                        color: theme.grey[50],
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+class OnboardingSection extends ConsumerWidget {
+  const OnboardingSection(
+      {super.key,
+      required this.icon,
+      required this.title,
+      required this.description});
+
+  final IconData icon;
+  final String title;
+  final String description;
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = CustomThemeInherited.of(context);
+    return Container(
+      padding: EdgeInsets.only(right: 32, left: 32),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Icon(
+            icon,
+            color: theme.primary[600],
+            weight: 100,
+          ),
+          horizontalSpace(Spacing.points16),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  title,
+                  style: TextStyles.h5.copyWith(
+                    color: theme.primary[600],
+                  ),
+                ),
+                verticalSpace(Spacing.points8),
+                // Second text
+                Text(
+                  description,
+                  style: TextStyles.footnote.copyWith(
+                    color: theme.grey[700],
+                  ),
+                ),
+              ],
+            ),
+          )
+        ],
+      ),
     );
   }
 }
