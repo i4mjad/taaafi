85e6817ae1364e8c8b7b3445e1e3c22d0dfe2a07
Amjad Khalfan <i4mjad@gmail.com>
2024-06-22 19:50:07 +0400

    feat, create forgot password email

---
+++ lib/features/authentication/presentation/forgot_password_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:reboot_app_3/core/localization/localization.dart';
+import 'package:reboot_app_3/core/shared_widgets/app_bar.dart';
+import 'package:reboot_app_3/core/shared_widgets/container.dart';
+import 'package:reboot_app_3/core/shared_widgets/custom_textfield.dart';
+import 'package:reboot_app_3/core/theming/app-themes.dart';
+import 'package:reboot_app_3/core/theming/spacing.dart';
+import 'package:reboot_app_3/core/theming/text_styles.dart';
+
+class ForgotPasswordScreen extends ConsumerStatefulWidget {
+  const ForgotPasswordScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _ForgotPasswordScreenState();
+}
+
+class _ForgotPasswordScreenState extends ConsumerState<ForgotPasswordScreen> {
+  final emailController = TextEditingController();
+
+  @override
+  void dispose() {
+    emailController.dispose();
+
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = CustomThemeInherited.of(context);
+    return Scaffold(
+      appBar: appBar(context, ref, 'forget-password', true),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            CustomTextField(
+              controller: emailController,
+              hint: AppLocalizations.of(context).translate('email'),
+              prefixIcon: LucideIcons.mail,
+              inputType: TextInputType.emailAddress,
+            ),
+            verticalSpace(Spacing.points32),
+            WidgetsContainer(
+              backgroundColor: theme.primary[600],
+              width: MediaQuery.of(context).size.width - (16 + 16),
+              padding: EdgeInsets.only(top: 12, bottom: 12),
+              borderRadius: BorderRadius.circular(10.5),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    AppLocalizations.of(context)
+                        .translate('send-reset-password-link'),
+                    style: TextStyles.footnoteSelected
+                        .copyWith(color: theme.grey[50]),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
