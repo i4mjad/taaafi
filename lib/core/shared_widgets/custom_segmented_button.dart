9c2e7470493708c0cba660a04d19b689ea7af9a2
Amjad Khalfan <i4mjad@gmail.com>
2024-06-22 11:47:33 +0400

    core, create shared widgets for textfields and segmented buttons

---
+++ lib/core/shared_widgets/custom_segmented_button.dart
@@ -0,0 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:reboot_app_3/core/theming/app-themes.dart';
+import 'package:reboot_app_3/core/theming/spacing.dart';
+import 'package:reboot_app_3/core/theming/text_styles.dart';
+
+class CustomSegmentedButton extends StatelessWidget {
+  final String label;
+  final List<String> options;
+  final String selectedOption;
+  final ValueChanged<String?> onChanged;
+
+  const CustomSegmentedButton({
+    Key? key,
+    required this.label,
+    required this.options,
+    required this.selectedOption,
+    required this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = CustomThemeInherited.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          label,
+          style: TextStyles.footnote.copyWith(
+            color: theme.grey[900],
+          ),
+        ),
+        verticalSpace(Spacing.points8),
+        Container(
+          width: MediaQuery.of(context).size.width,
+          child: LayoutBuilder(
+            builder: (context, constraints) {
+              return ToggleButtons(
+                renderBorder: true,
+                constraints: BoxConstraints.expand(
+                  width: (constraints.maxWidth - (options.length + 2)) /
+                      options.length,
+                ),
+                borderRadius: BorderRadius.circular(5),
+                children: options
+                    .map((option) => Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                          child: Text(
+                            option,
+                            style: TextStyles.body,
+                          ),
+                        ))
+                    .toList(),
+                isSelected: options.map((e) => e == selectedOption).toList(),
+                onPressed: (int index) {
+                  onChanged(options[index]);
+                },
+                selectedBorderColor: theme.primary[600],
+                selectedColor: theme.grey[50],
+                fillColor: theme.primary[600],
+                color: theme.primary[600],
+              );
+            },
+          ),
+        ),
+      ],
+    );
+  }
+}
---
+++ lib/core/shared_widgets/custom_textfield.dart
@@ -0,0 +1,95 @@
+import 'package:figma_squircle/figma_squircle.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:reboot_app_3/core/theming/app-themes.dart';
+import 'package:reboot_app_3/core/theming/spacing.dart';
+import 'package:reboot_app_3/core/theming/text_styles.dart';
+
+class CustomTextField extends ConsumerWidget {
+  final TextEditingController controller;
+  final String? hint;
+  final IconData prefixIcon;
+  final bool obscureText;
+  final TextCapitalization textCapitalization;
+  final TextInputType inputType;
+  final BorderRadius? borderRadius;
+  final BorderSide? borderSide;
+  final double? width;
+
+  const CustomTextField({
+    Key? key,
+    this.borderRadius,
+    this.borderSide,
+    this.width,
+    required this.controller,
+    this.hint,
+    required this.prefixIcon,
+    this.obscureText = false,
+    this.textCapitalization = TextCapitalization.none,
+    required this.inputType,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final theme = CustomThemeInherited.of(context);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Builder(
+          builder: (BuildContext context) {
+            if (hint != null) {
+              return Column(
+                children: [
+                  Text(
+                    hint!,
+                    style: TextStyles.footnote,
+                  ),
+                  verticalSpace(Spacing.points4),
+                ],
+              );
+            }
+            return SizedBox.shrink();
+          },
+        ),
+        Container(
+          width: width ?? MediaQuery.of(context).size.width - 32,
+          decoration: ShapeDecoration(
+            color: theme.grey[50],
+            shape: SmoothRectangleBorder(
+              borderRadius: SmoothBorderRadius(
+                cornerRadius:
+                    (borderRadius ?? BorderRadius.circular(10.5)).topLeft.x,
+                cornerSmoothing: 1,
+              ),
+              side: borderSide ??
+                  BorderSide(
+                    color: theme.grey[100]!,
+                    width: 1,
+                  ),
+            ),
+          ),
+          child: TextField(
+            enabled: true,
+            controller: controller,
+            textCapitalization: textCapitalization,
+            maxLength: 100,
+            maxLines: 1,
+            obscureText: obscureText,
+            keyboardType: inputType,
+            textAlign: TextAlign.start,
+            style: TextStyles.footnote,
+            decoration: InputDecoration(
+              prefixIcon: Icon(
+                prefixIcon,
+                color: theme.primary[600],
+              ),
+              counterText: "",
+              border: InputBorder.none,
+              contentPadding: EdgeInsets.only(top: 16, bottom: 16),
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
