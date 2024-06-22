ddc15b114262ed8f0633239a2d1e1c6c6b6999c5
Amjad Khalfan <i4mjad@gmail.com>
2024-06-22 18:24:32 +0400

    cleaup, remove unused imports

--- lib/features/onboarding/presentation/onboarding_screen.dart
+++ lib/features/onboarding/presentation/onboarding_screen.dart
@@ -7,7 +7,6 @@ import 'package:reboot_app_3/core/routing/route_names.dart';
 import 'package:reboot_app_3/core/shared_widgets/app_bar.dart';
 import 'package:reboot_app_3/core/shared_widgets/container.dart';
 import 'package:reboot_app_3/core/theming/app-themes.dart';
-import 'package:reboot_app_3/core/theming/custom_theme_data.dart';
 import 'package:reboot_app_3/core/theming/spacing.dart';
 import 'package:reboot_app_3/core/theming/text_styles.dart';
 
