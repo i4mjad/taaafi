d3a006c81f04af3a20f43128321948921fdea050
Amjad Khalfan <i4mjad@gmail.com>
2024-06-23 12:21:45 +0400

    feat wip, create complete account registration page

---
+++ lib/features/authentication/presentation/complete_account_registeration.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+class CompleteAccountRegisterationScreen extends ConsumerStatefulWidget {
+  const CompleteAccountRegisterationScreen({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() =>
+      _CompleteAccountRegisterationScreenState();
+}
+
+class _CompleteAccountRegisterationScreenState
+    extends ConsumerState<CompleteAccountRegisterationScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SingleChildScrollView(
+        child: Column(
+          children: [],
+        ),
+      ),
+    );
+  }
+}
