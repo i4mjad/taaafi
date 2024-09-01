942c47c2afb815d4d84029c8b4f443e26be42c4f
Amjad Khalfan <i4mjad@gmail.com>
2024-09-01 17:24:12 +0400

    feat, add the fab to home

--- lib/features/home/presentation/home_screen.dart
+++ lib/features/home/presentation/home_screen.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:lucide_icons/lucide_icons.dart';
+import 'package:reboot_app_3/core/localization/localization.dart';
 import 'package:reboot_app_3/core/shared_widgets/app_bar.dart';
 import 'package:reboot_app_3/core/theming/app-themes.dart';
+import 'package:reboot_app_3/core/theming/text_styles.dart';
 
 class HomeScreen extends ConsumerWidget {
   const HomeScreen({super.key});
@@ -24,6 +27,16 @@ class HomeScreen extends ConsumerWidget {
           ),
         ),
       ),
+      floatingActionButton: FloatingActionButton.extended(
+          backgroundColor: theme.primary[600],
+          onPressed: () {
+            // TODO: open the followup modal
+          },
+          label: Text(
+            AppLocalizations.of(context).translate("daily-follow-up"),
+            style: TextStyles.caption.copyWith(color: theme.grey[50]),
+          ),
+          icon: Icon(LucideIcons.pencil, color: theme.grey[50])),
     );
   }
 }
