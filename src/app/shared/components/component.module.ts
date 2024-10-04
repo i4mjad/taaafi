4333ccc9cf293c299d64ee30ea46be5b65038dda
Amjad Khalfan <i4mjad@gmail.com>
2024-10-04 11:41:29 +0400

    refactor wip, migerate the website to use the spartan components

--- src/app/shared/components/component.module.ts
+++ src/app/shared/components/component.module.ts
@@ -1,13 +1,29 @@
 import { NgModule } from "@angular/core";
+import { provideIcons } from "@ng-icons/core";
 import { HlmButtonDirective } from "@spartan-ng/ui-button-helm";
+import { HlmCardContentDirective, HlmCardDescriptionDirective, HlmCardDirective, HlmCardFooterDirective, HlmCardHeaderDirective, HlmCardTitleDirective } from "@spartan-ng/ui-card-helm";
+import { HlmIconComponent } from "@spartan-ng/ui-icon-helm";
+import { lucideChevronRight, lucideUsers } from '@ng-icons/lucide';
+
 
 
 const components = [
     HlmButtonDirective,
+    HlmCardContentDirective,
+    HlmCardDescriptionDirective,
+    HlmCardDirective,
+    HlmCardFooterDirective,
+    HlmCardHeaderDirective,
+    HlmCardTitleDirective,
+    HlmIconComponent,
+
 ]
 
 @NgModule({
     imports: components,
+    providers:[
+        provideIcons({ lucideChevronRight,lucideUsers })
+    ],
     exports: components,
 })
 export class SpartanCompnentsModule {}
