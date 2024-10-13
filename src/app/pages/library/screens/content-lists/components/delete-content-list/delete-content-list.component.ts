d160517a255120d304bf084108482ec1c07b14b1
Amjad Khalfan <i4mjad@gmail.com>
2024-10-13 15:09:40 +0400

    feat, add delete content list feature

--- src/app/pages/library/screens/content-lists/components/delete-content-list/delete-content-list.component.ts
+++ src/app/pages/library/screens/content-lists/components/delete-content-list/delete-content-list.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+import { Content, ContentList } from '../../../../../../models/app.model';
 
 @Component({
   selector: 'app-delete-content-list',
   templateUrl: './delete-content-list.component.html',
-  styleUrl: './delete-content-list.component.scss'
+  styleUrl: './delete-content-list.component.scss',
 })
 export class DeleteContentListComponent {
+  constructor(
+    public dialogRef: MatDialogRef<DeleteContentListComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: { contentList: ContentList }
+  ) {}
 
+  onCancel(): void {
+    this.dialogRef.close(false);
+  }
+
+  onConfirm(): void {
+    this.dialogRef.close(true);
+  }
 }
