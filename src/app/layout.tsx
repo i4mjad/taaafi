12f67518ecedcb180e0165c9e4c9e7645a06619d
Amjad Khalfan <i4mjad@gmail.com>
2025-06-14 15:58:31 +0400

    chore, add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,21 @@
-import type { Metadata } from "next";
+import type { Metadata, Viewport } from "next";
 import "@/app/globals.css";
+
+export const viewport: Viewport = {
+  themeColor: '#FFFFFF',
+};
 
 export const metadata: Metadata = {
   title: "Ta'aafi App",
   description: "Ta'aafi App Website",
+  icons: {
+    icon: [
+      { url: '/favicon.ico', sizes: 'any' },
+      { url: '/favicon.svg', type: 'image/svg+xml' }
+    ],
+    apple: '/apple-touch-icon.png',
+  },
+  manifest: '/site.webmanifest',
 };
 
 export default function RootLayout({
